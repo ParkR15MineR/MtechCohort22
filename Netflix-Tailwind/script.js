0c77fdb06aaf6a66409e338d99cf062c300afc3f
ParkR15MineR <parkerlamb@gmail.com>
2026-01-29 19:12:19 -0700

    made the code for netflix

---
+++ Netflix-Tailwind/script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+
+theme: {
+  extend: {
+    colors: {
+      'netflix-red': '#E50914',
+      'glass-white': 'rgba(255, 255, 255, 0.05)',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  }
+}}
+export default config;
